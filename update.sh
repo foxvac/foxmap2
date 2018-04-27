1dc3619720ddc6815f4db10e5861224c30d7c9d6
foxvac <38643902+foxvac@users.noreply.github.com>
2018-04-27 23:12:41 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ echo "欢迎使用网页雷达一键脚本"
 echo "即将搭建FOX4.25版本"
 echo "请输入你的内网ip" 
 read -p "内网ip： " ip
-cp /root/foxmap/restart.sh /root/restart.sh
+cp /root/foxmap2/restart.sh /root/restart.sh
 chmod +x restart.sh
 wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
@@ -26,8 +26,8 @@ cd libpcap-1.8.1
 make
 make install
 
-git clone https://github.com/foxvac/foxmap.git
-cd foxmap/
+git clone https://github.com/foxvac/foxmap2.git
+cd foxmap2/
 npm i
 npm i -g pino
 npm install -g forever
